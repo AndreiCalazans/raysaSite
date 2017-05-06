61d687fa66612922dd66433d3227a2e1e88d4832
AndreiCalazans <andreixoc@hotmail.com>
2017-05-06 06:41:59 -0300

    first implement of fade in when on scroll animation

--- dist/bundle.js
+++ dist/bundle.js
@@ -83,6 +83,7 @@ __webpack_require__(0);
 
 $(document).ready(function () {
 
+    //Side Nav script\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
     $('.fa-bars').on('click', function () {
         $('.sideNav').toggleClass('showSideNav');
     });
@@ -91,6 +92,24 @@ $(document).ready(function () {
         if ($('.sideNav').hasClass('showSideNav')) {
             $('.sideNav').removeClass('showSideNav');
         }
+    });
+
+    // Fade in things script \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    $(window).scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.hideme').each(function (i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 500);
+            }
+        });
     });
 });
 
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import './style.scss';
 $(document).ready(function() {
 
 
-  
+//Side Nav script\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
     $('.fa-bars').on('click', function() {
         $('.sideNav').toggleClass('showSideNav');
     });
@@ -13,6 +13,28 @@ $(document).ready(function() {
             $('.sideNav').removeClass('showSideNav');
         }
     })
+
+// Fade in things script \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+     $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + ($(this).outerHeight()/2);
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+
 
 
 });
