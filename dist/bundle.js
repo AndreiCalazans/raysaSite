65684fabdb59a774f6f79b335c8af9c4557aa06d
AndreiCalazans <andreixoc@hotmail.com>
2017-05-05 21:02:10 -0300

    Added sideNav

--- dist/bundle.js
+++ dist/bundle.js
@@ -81,5 +81,18 @@
 
 __webpack_require__(0);
 
+$(document).ready(function () {
+
+    $('.fa-bars').on('click', function () {
+        $('.sideNav').toggleClass('showSideNav');
+    });
+
+    $('section').on('click', function () {
+        if ($('.sideNav').hasClass('showSideNav')) {
+            $('.sideNav').removeClass('showSideNav');
+        }
+    });
+});
+
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
 import './style.scss';
 
+$(document).ready(function() {
+
+
+  
+    $('.fa-bars').on('click', function() {
+        $('.sideNav').toggleClass('showSideNav');
+    });
+
+    $('section').on('click', function(){
+        if($('.sideNav').hasClass('showSideNav')) {
+            $('.sideNav').removeClass('showSideNav');
+        }
+    })
+
+
+});
