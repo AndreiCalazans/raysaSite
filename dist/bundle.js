dd86afe8b3e1ac54a8af4f98907a766335a8d877
AndreiCalazans <andreixoc@hotmail.com>
2017-05-11 09:50:23 -0300

    reversed the parallax feature due to incompatability with phones

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,162 @@
-!function(t){function o(n){if(e[n])return e[n].exports;var i=e[n]={i:n,l:!1,exports:{}};return t[n].call(i.exports,i,i.exports,o),i.l=!0,i.exports}var e={};o.m=t,o.c=e,o.i=function(t){return t},o.d=function(t,e,n){o.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:n})},o.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return o.d(e,"a",e),e},o.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)},o.p="/dist",o(o.s=1)}([function(t,o){},function(t,o,e){"use strict";e(0),$(document).ready(function(){$(".fa-bars").on("click",function(){$(".sideNav").toggleClass("showSideNav")}),$("section").on("click",function(){$(".sideNav").hasClass("showSideNav")&&$(".sideNav").removeClass("showSideNav")}),$(window).scroll(function(){$(window).scrollTop()>=$("#areas").offset().top?$("#to-top-btn").css("display","inherit"):(console.log("helelo"),$("#to-top-btn").css("display","none")),$(".hideme").each(function(t){var o=$(this).offset().top+$(this).outerHeight()/3;$(window).scrollTop()+$(window).height()>o&&$(this).animate({opacity:"1"},500)})}),$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(t){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var o=$(this.hash);o=o.length?o:$("[name="+this.hash.slice(1)+"]"),o.length&&(t.preventDefault(),$("html, body").animate({scrollTop:o.offset().top},1e3,function(){var t=$(o);if(t.focus(),t.is(":focus"))return!1;t.attr("tabindex","-1"),t.focus()}))}})})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/dist";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(0);
+
+$(document).ready(function () {
+
+    //Side Nav script\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  
+    $('.fa-bars').on('click', function () {
+        $('.sideNav').toggleClass('showSideNav');
+    });
+
+    $('section').on('click', function () {
+        if ($('.sideNav').hasClass('showSideNav')) {
+            $('.sideNav').removeClass('showSideNav');
+        }
+    });
+
+    // Fade in things script \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    $(window).scroll(function () {
+
+        //LOGIC TO MAKE GO BACK TO THE TOP BUTTON APPEAR
+        var top_of_window = $(window).scrollTop();
+        var start_of_areas = $('#areas').offset().top;
+
+        if (top_of_window >= start_of_areas) {
+            //show the button
+            $('#to-top-btn').css('display', 'inherit');
+        } else {
+            console.log('helelo');
+            $('#to-top-btn').css('display', 'none');
+        }
+
+        /* Check the location of each desired element */
+        $('.hideme').each(function (i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() / 3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 500);
+            }
+        });
+    });
+
+    // SMOOTH SCROLL PLUGGING FROM CSS TRICKS
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]').not('[href="#0"]').click(function (event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+});
+
+/***/ })
+/******/ ]);
